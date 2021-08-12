5fa828e977e68523c0ec4938a55530c7bc97fcf0
ngmachado1 <nataliagisellemachado@gmail.com>
2021-08-12 14:59:46 -0300

    actualizando repositorio

--- view/usuario/usuario.php
+++ view/usuario/usuario.php
@@ -1,9 +1,10 @@
 ﻿<h2>Hola! <?= $_SESSION['Correo']; ?></h2>
 
 
-<table class="table  table-striped  table-hover" id="tabla">
+<table class="table" id="tabla">
     <thead>
         <tr>
+            <th>Nombre</th>
             <th>Nombre</th>
             <th>Apellido</th>
             <th>Correo</th>
@@ -13,10 +14,11 @@
     <tbody>
     <?php foreach($this->model->Listar() as $r): ?>
         <tr>
-            <td><?= $r->id . "-" . $r->Nombre; ?></td>
+            <td><?= $r->id; ?></td>
+            <td><?= $r->Nombre; ?></td>
             <td><?= $r->Apellido; ?></td>
             <td><?= $r->Correo; ?></td>
-            <td>
+            <td class="d-flex">
                 <a  class="btn btn-warning" href="?c=usuario&a=Crud&id=<?= $r->id; ?>">Editar</a>
                 <a  class="btn btn-danger" onclick="javascript:return confirm('¿Seguro de eliminar este registro?');" href="?c=usuario&a=Eliminar&id=<?php echo $r->id; ?>">Eliminar</a>
             </td>
@@ -24,8 +26,11 @@
     <?php endforeach; ?>
     </tbody>
 </table>     
+<div class="center">
 <a class="btn btn-primary" href="?c=usuario&a=Crud">Agregar</a>
 <a href="?c=usuario&a=Logout" class="btn btn-secondary">salir</a>
+
+</div>
 </body>
 
 </script>
